0d625717ecf9012decdbce159a6fee286600ba8f
ABashir01 <ahadbashirblast@gmail.com>
2023-11-24 03:04:18 -0600

    Fixed issues with socket io rendering, now goes to lobby as expected

--- server/index.js
+++ server/index.js
@@ -22,16 +22,23 @@ server.listen(3001, () => {
 })
 
 io.on("connection", (socket) => {
-    socket.on("create_lobby", () => {
+    socket.on("create_lobby", (callback) => {
         let roomCode = randomInt(0,9999);
         while (activeRooms.has(roomCode)) {
             roomCode = randomInt(0,9999);
         }
+
+        // let playersSet = new Set();
+        // playersSet.add(socket);
+        // activeRooms.set(roomCode, playersSet);
         activeRooms.add(roomCode);
+        
 
         socket.join(roomCode);
         console.log("Lobby Time");
+        console.log(activeRooms);
 
-        socket.emit("receive_code", roomCode);
+        // socket.emit("receive_code", roomCode);
+        callback(roomCode);
     });
 });
